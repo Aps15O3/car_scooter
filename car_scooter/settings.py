b08329834ec25a7689dcb277de71ff39a1fd9045
Aps15O3 <96794885+Aps15O3@users.noreply.github.com>
2022-11-10 19:18:21 +0530

    static

--- car_scooter/settings.py
+++ car_scooter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_nujwm&h(l@@4^nej0s7js)@rdy-!$x(&xxh1b(@uq^r(h+2%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["carscooterpricepredictor.herokuapp.com"]
 
 
 # Application definition
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import django_on_heroku
 django_on_heroku.settings(locals())
